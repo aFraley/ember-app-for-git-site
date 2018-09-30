f780de61dd12520dc43602501c725351f78a70f6
Alan Fraley <aFraley@users.noreply.github.com>
2018-09-30 13:51:35 -0500

    Development (#1)
    * update
    
    * undo output-path
    
    * update

---
+++ app/controllers/index.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+    title: 'Software Engineer',
+    technology: [
+        'Python and Django',
+        'Vue',
+        'React and React Native',
+        'Ember',
+        'Elasticsearch',
+        'Golang and Gin',
+        'Java and Spring',
+        'C',
+        'AWS'
+    ]
+});
