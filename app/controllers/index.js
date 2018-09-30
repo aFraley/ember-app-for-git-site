aecc1cc59fc9c7ac1f849d2acb570a7ed7adb14e
aFraley <humanimalmammal@webtv.net>
2018-09-30 12:37:24 -0500

    update

---
+++ app/controllers/index.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+    message: 'I am a Software Engineer at Lofty Labs.',
+    technology: [
+        'Python and Django',
+        'Vue',
+        'React and React Native',
+        'Ember',
+        'Elasticsearch',
+        'Golang and Gin',
+        'Java and Spring',
+        'C',
+        'AWS'
+    ]
+});
